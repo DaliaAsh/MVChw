15ba3a78408da475b1228b37d795573835ed1af6
HP <HP@DESKTOP-BO9O40E>
2019-04-06 14:32:02 +0300

    commite

--- src/daliaTest/Test1.java
+++ src/daliaTest/Test1.java
@@ -1,14 +1,25 @@
 package daliaTest;
-
 import static org.junit.Assert.*;
-
 import org.junit.Test;
+import source.Model;
 
 public class Test1 {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		Model m = new Model();
+		m.addTwoNumbers(10.0,8.0);
+		assertTrue(18 == m.getCalcValue()); 
+	    m.SubTwoNumbers(5,2);
+	    assertTrue(3 == m.getCalcValue());
+	    m.MulTwoNumbers(5,5);
+	    assertTrue(25 == m.getCalcValue());
+	    m.DivTwoNumbers(9,3);
+	    assertTrue(3 == m.getCalcValue());
+	    m.ModTwoNumbers(9,3);
+	    assertTrue(0 == m.getCalcValue());
+	    m.SqtNumber(16);
+	    assertTrue(4 == m.getCalcValue());
 	}
 
 }
